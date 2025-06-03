b282065590c57d29e6b548867af80159a9984cb6
Jyotin Goel <b22ai063@iitj.ac.in>
2025-06-04 02:36:47 +0530

    site setup
    Signed-off-by: Jyotin Goel <b22ai063@iitj.ac.in>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://aisocietyiitj.github.io',
+	base: 'blogs-raid',
 	integrations: [mdx(), sitemap()],
 });
