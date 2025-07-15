d608193af8182e50609584e567ce998da1381b92
isonam16 <acinonyx11ok@gmail.com>
2025-07-15 15:54:28 +0530

    final changes in st_diff_blog.mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,26 @@
+// // @ts-check
+// import { defineConfig } from "astro/config";
+// import mdx from "@astrojs/mdx";
+// import sitemap from "@astrojs/sitemap";
+
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://astro.build/config
+// export default defineConfig({
+//   site: "https://aisocietyiitj.github.io/blogs-raid/",
+//   base: "/blogs-raid",
+//   integrations: [mdx(), sitemap()],
+
+//   vite: {
+//     plugins: [tailwindcss()],
+//   },
+// });
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 import tailwindcss from "@tailwindcss/vite";
 
@@ -9,7 +28,13 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   site: "https://aisocietyiitj.github.io/blogs-raid/",
   base: "/blogs-raid",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+    sitemap(),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
