1a0f3c79839f17cee05f725923e408f79567f4c8
Vishal Kishore <kishorevishal01@gmail.com>
2025-07-15 12:03:18 +0530

    Feature: add support for math rendering with KaTeX and other utilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,20 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import remarkMath from 'remark-math';
+import remarkGfm from 'remark-gfm';
+import rehypeKatex from 'rehype-katex';
+import rehypeMdxImportMedia from 'rehype-mdx-import-media';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://aisocietyiitj.github.io/blogs-raid/",
   base: "/blogs-raid",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx({
+    remarkPlugins: [remarkMath,remarkGfm],
+    rehypePlugins: [rehypeKatex, rehypeMdxImportMedia],
+  }), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
